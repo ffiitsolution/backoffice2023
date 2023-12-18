bc4e45d6a81694a9cd687b4fb89c34630a4f921c
Fathur Rizko <kaukanoer@gmail.com>
2023-12-18 10:59:54 +0700

    Added date time Upd to mpcs production list endpoint - Fathur 18 Dec

--- src/main/java/com/ffi/api/backoffice/dao/impl/ViewDoaImpl.java
+++ src/main/java/com/ffi/api/backoffice/dao/impl/ViewDoaImpl.java
@@ -3155,7 +3155,8 @@ public class ViewDoaImpl implements ViewDao {
     @Override
     public List<Map<String, Object>> mpcsProductionList(Map<String, String> balance) {
 
-        String qry = "select to_char(to_date(c.TIME_MPCS, 'hh24miss'), 'hh24:mi') as TIME_MPCS, c.QTY_PROD, c.QTY_ACC_PROD, c.QTY_ACC_PROD, NVL(c.DESC_PROD, ' ') AS DESC_PROD, c.PROD_BY "
+        String qry = "select to_char(to_date(c.TIME_MPCS, 'hh24miss'), 'hh24:mi') as TIME_MPCS, c.QTY_PROD, c.QTY_ACC_PROD, c.QTY_ACC_PROD, NVL(c.DESC_PROD, ' ') AS DESC_PROD, c.PROD_BY, "
+                + "(to_char(DATE_UPD, 'YYYY-MM-dd') || ' ' || to_char(to_date(TIME_UPD, 'hh24miss'), 'hh24:mi:ss')) AS DATE_UPD "
                 + "from t_summ_mpcs c "
                 + "where c.date_mpcs = :dateMpcs "
                 + "AND c.MPCS_GROUP = :mpcsGroup";
@@ -3174,6 +3175,7 @@ public class ViewDoaImpl implements ViewDao {
                 rt.put("qtyAccProd", rs.getString("QTY_ACC_PROD"));
                 rt.put("descProd", rs.getString("DESC_PROD"));
                 rt.put("prodBy", rs.getString("PROD_BY"));
+                rt.put("dateUpd", rs.getString("DATE_UPD"));
 
                 return rt;
             }
