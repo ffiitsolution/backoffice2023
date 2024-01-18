912840d5e05c5bed3d1220b2da9ca762686834f0
M Joko M <40954176+hijoem@users.noreply.github.com>
2024-01-18 17:01:59 +0700

    fix list menu from env FE

--- src/main/java/com/ffi/api/backoffice/IndexController.java
+++ src/main/java/com/ffi/api/backoffice/IndexController.java
@@ -48,7 +48,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class IndexController {
-    
+
     @Autowired
     ViewServices viewServices;
     @Autowired
@@ -1665,8 +1665,8 @@ public class IndexController {
         String qtyPurch = balance.getOrDefault("qtyPurch", "0");
         String qtyStock = balance.getOrDefault("qtyStock", "0");
         String totalQty = balance.getOrDefault("totalQty", "0");
-        if (Long.parseLong(qtyPurch) > maxValue || Long.parseLong(qtyStock) > maxValue || Long.parseLong(totalQty) > maxValue || 
-            qtyPurch.length() > 14 || qtyStock.length() > 14 || totalQty.length() > 14) {
+        if (Long.parseLong(qtyPurch) > maxValue || Long.parseLong(qtyStock) > maxValue || Long.parseLong(totalQty) > maxValue
+                || qtyPurch.length() > 14 || qtyStock.length() > 14 || totalQty.length() > 14) {
             rm.setSuccess(false);
             rm.setMessage("Nilai maksimum terlampaui, mohon cek kembali jumlah item.");
             return rm;
@@ -2428,12 +2428,12 @@ public class IndexController {
         ResponseMessage rm = new ResponseMessage();
         try {
             list = processServices.updateOpnameStatus(balance);
-            if(list.isEmpty()){
+            if (list.isEmpty()) {
                 rm.setSuccess(true);
                 rm.setMessage("Update Success");
-            }else {
+            } else {
                 rm.setSuccess(false);
-                rm.setMessage( list.size() + " item berikut terdeteksi memiliki nilai input total 0");
+                rm.setMessage(list.size() + " item berikut terdeteksi memiliki nilai input total 0");
             }
         } catch (Exception e) {
             rm.setSuccess(false);
@@ -3181,20 +3181,21 @@ public class IndexController {
         Map<String, Object> data = gsn.fromJson(param, new TypeToken<Map<String, String>>() {
         }.getType());
         // List<Map<String, Object>> list = viewServices.listMenuApplication(data);
-        String env = appUtil.getOrDefault("app.env", "development");
+        String env = data.containsKey("env") ? data.getOrDefault("env", "prod").toString() : appUtil.getOrDefault("app.env", "development");
         Response res = new Response();
-        String filepath = env.equalsIgnoreCase("development") ? "json/menuKFC-dev.json" : "json/menuKFC.json";
-        if(data.containsKey("outletBrand") && data.get("outletBrand").toString().equalsIgnoreCase("TACOBELL")){
+        String filepath = env.toLowerCase().contains("dev") ? "json/menuKFC-dev.json" : "json/menuKFC.json";
+        System.err.println("menu filepath: " + filepath);
+        if (data.containsKey("outletBrand") && data.get("outletBrand").toString().equalsIgnoreCase("TACOBELL")) {
             filepath = "json/menuTACOBELL.json";
         }
-        try{
+        try {
             ClassPathResource resource = new ClassPathResource(filepath);
             byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
             String jsonString = new String(bytes, StandardCharsets.UTF_8);
             ObjectMapper objectMapper = new ObjectMapper();
             List<Map<String, Object>> menuList = objectMapper.readValue(jsonString, new ArrayList<LinkedHashMap<String, Object>>().getClass());
             res.setData(menuList);
-        } catch (IOException e){
+        } catch (IOException e) {
             System.err.println("e: " + e.getMessage());
             List<Map<String, Object>> menuList = new ArrayList();
             data.put("success", false);
@@ -3235,7 +3236,8 @@ public class IndexController {
         @ApiResponse(code = 200, message = "OK"),
         @ApiResponse(code = 404, message = "The resource not found"),}
     )
-    public @ResponseBody Response listCustomerNameReportDp(@RequestBody String param) throws IOException, Exception {
+    public @ResponseBody
+    Response listCustomerNameReportDp(@RequestBody String param) throws IOException, Exception {
         Response res = new Response();
         res.setData(viewServices.listCustomerNameReportDp());
         return res;
@@ -3248,12 +3250,13 @@ public class IndexController {
         @ApiResponse(code = 200, message = "OK"),
         @ApiResponse(code = 404, message = "The resource not found"),}
     )
-    public @ResponseBody Response listOrderTypeReportDp(@RequestBody String param) throws IOException, Exception {
+    public @ResponseBody
+    Response listOrderTypeReportDp(@RequestBody String param) throws IOException, Exception {
         Response res = new Response();
         res.setData(viewServices.listOrderTypeReportDp());
         return res;
     }
-    
+
     @RequestMapping(value = "/delete-mpcs-production", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Digunakan untuk menghapus mpcs produksi detail by Fathur 11 Jan 2024", response = Object.class)
     @ApiResponses(value = {
@@ -3275,7 +3278,7 @@ public class IndexController {
         }
         return rm;
     }
-    
+
     @RequestMapping(value = "/get-id-absensi", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Digunakan untuk mengambil data user absensi by id by M Joko 16 Jan 2024", response = Object.class)
     @ApiResponses(value = {
@@ -3289,11 +3292,11 @@ public class IndexController {
         }.getType());
         ResponseMessage rm = new ResponseMessage();
         List<Map<String, Object>> list = viewServices.getIdAbsensi(data);
-        if(!list.isEmpty()){
+        if (!list.isEmpty()) {
             Map first = list.get(0);
             rm.setItem(list);
             rm.setSuccess(true);
-            rm.setMessage(((first.get("daySeq").toString().equalsIgnoreCase("0") && first.get("seqNo").toString().equalsIgnoreCase("0")) || list.size() % 2 == 0 ? "Masuk" : ( list.size() % 2 != 0 ? "Keluar" : "User Found")));
+            rm.setMessage(((first.get("daySeq").toString().equalsIgnoreCase("0") && first.get("seqNo").toString().equalsIgnoreCase("0")) || list.size() % 2 == 0 ? "Masuk" : (list.size() % 2 != 0 ? "Keluar" : "User Found")));
             return rm;
         }
         rm.setSuccess(false);
@@ -3301,7 +3304,7 @@ public class IndexController {
         rm.setItem(new ArrayList());
         return rm;
     }
-    
+
     @RequestMapping(value = "/insert-absensi", produces = MediaType.APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Digunakan untuk menyimpan data user absensi by id by M Joko 16 Jan 2024", response = Object.class)
     @ApiResponses(value = {
@@ -3317,7 +3320,7 @@ public class IndexController {
         boolean b = processServices.insertAbsensi(data);
         rm.setItem(new ArrayList());
         rm.setSuccess(b);
-        rm.setMessage( b ? "Absensi berhasil." : "Periksa kembali password anda.");
+        rm.setMessage(b ? "Absensi berhasil." : "Periksa kembali password anda.");
         return rm;
     }
 }
