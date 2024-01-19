6660c3f0bfe172ac2a77f8c1483425d086073919
Pasca <pascaramadhan28@gmail.com>
2024-01-19 15:06:43 +0700

    update bug fix by pasca tanggal 19-01-2024

--- src/main/java/com/ffi/api/backoffice/dao/impl/ViewDoaImpl.java
+++ src/main/java/com/ffi/api/backoffice/dao/impl/ViewDoaImpl.java
@@ -1379,7 +1379,7 @@ public class ViewDoaImpl implements ViewDao {
 
     @Override
     public List<Map<String, Object>> listGlobal(Map<String, String> balance) {
-        String qry = "SELECT DESCRIPTION, TYPE_MENU AS CODE FROM m_menudtl WHERE TYPE_MENU = :cond AND STATUS = :status AND APLIKASI = :aplikasi";
+        String qry = "SELECT DESCRIPTION, TYPE_MENU AS CODE FROM m_menudtl WHERE TYPE_MENU = :cond AND STATUS = :status AND APLIKASI = :aplikasi ORDER BY ID_NO ASC";
         if (balance.containsKey("outletBrand") && balance.get("outletBrand").equalsIgnoreCase("TACOBELL")) {
             qry += " AND DESCRIPTION IN ('Report Menu & Detail Modifier', 'Sales by Date', 'Sales by Item', 'Sales by Time', 'Summary Sales by Item Code', 'Report Stock Card')";
         }
