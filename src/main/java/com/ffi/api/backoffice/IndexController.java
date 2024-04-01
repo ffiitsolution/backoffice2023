9284951a8447fd9f7607b1a0ea9003f67ef82712
DaniRamadhan <61649124+DaniRamadhan@users.noreply.github.com>
2024-04-01 10:46:19 +0700

    oil usage master

--- src/main/java/com/ffi/api/backoffice/IndexController.java
+++ src/main/java/com/ffi/api/backoffice/IndexController.java
@@ -4373,9 +4373,9 @@ public class IndexController {
     }    
     /////////////////// end aditya 22 Mar 2024    
 
-        /////////////// new method from aditya 22-03-2024////////////////////////////
+    /////////////// new method from dani 1 April 2024////////////////////////////
     @RequestMapping(value = "/list-oil-usage", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ApiOperation(value = "Digunakan untuk view List Level", response = Object.class)
+    @ApiOperation(value = "Digunakan untuk view List Oil Usage", response = Object.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "OK"),
             @ApiResponse(code = 404, message = "The resource not found"), })
