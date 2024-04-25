28c7756bc09cdf984df433af47a90777b9b6d809
M Joko M <40954176+hijoem@users.noreply.github.com>
2024-04-25 14:13:29 +0700

    add validasi kode staff dan pos Master User, update koneksi websocket - M Joko 25-4-24

--- src/main/java/com/ffi/api/backoffice/utils/WebSocketConfig.java
+++ src/main/java/com/ffi/api/backoffice/utils/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
+        registry.addEndpoint("/sockjs").setAllowedOriginPatterns("*").withSockJS();
     }
 }
